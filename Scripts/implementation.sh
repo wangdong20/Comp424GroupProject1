7db294e18258d1c5fe7c71dd2fdf5fbaf4e07d5f
topheryun <46732742+topheryun@users.noreply.github.com>
2020-03-27 15:14:23 -0700

    Scripts to install and implement firewall

---
+++ Scripts/implementation.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+##################################################
+# implementation.sh: It will include all the commands regarding implementing policies for your firewall, IDS, etc. You may use a stream editor such as “sed” to implement them using your automated scripts, if that requires editing specific files
+
+# Written by Chris Yun
+
+##################################################
+
+# Basic firewall with IPTables:
+
+# "Check the status of your current iptables configuration"
+echo -e "Checking IPTables status.."
+sudo iptables -L -v
+
+# allow traffic on localhost
+echo -e "Allowing traffic on localhost.."
+sudo iptables -A INPUT -i lo -j ACCEPT
+
+# Enable connections on HTTP, SSH, SSL
+echo -e "Enabling SSH(22) port.."
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+echo -e "Enabling HTTP(80) port.."
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+
+echo -e "Enabling HTTPS(443) port.."
+sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# check if rules were appended
+sudo iptables -L -v
+
+# dropping all other traffic other than dport
+echo -e "Adding rule to drop unauthorized connections.."
+sudo iptables -A INPUT -j DROP
+
+# Persisting Changes
+echo -e "Saving changes to IPTables.."
+sudo /sbin/iptables-save
+
+echo -e "Finished implementing IPTables Firewall rules."
+
+##################################################
+
+# RESOURCES
+
+# IPTables setup:
+# https://www.hostinger.com/tutorials/iptables-tutorial
